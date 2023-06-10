c8298fd22d5c626399fbcb857fe5f02ac0e9b7d1
Joan Casimero <casimerojoan@gmail.com>
2023-06-10 11:18:18 +0800

    Update PaintsController.java

--- controller/PaintsController.java
+++ controller/PaintsController.java
@@ -1,7 +1,11 @@
 package controller;
 
+import model.*;
+import java.net.URL;
 import java.io.IOException;
+import java.util.ResourceBundle;
 
+import alert.AlertMaker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,11 +20,30 @@ import javafx.stage.Stage;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.control.ContentDisplay;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
-public class PaintsController {
+public class PaintsController implements Initializable {
 
     @FXML
-    Button logo;
+    Button logo, button1, button2, button3, button4, cartbutton;;
+
+     @FXML
+    Label name1, name2, name3, name4, price1, price2, price3, price4;
+
+      @FXML
+    ImageView img1, img2, img3, img4;
+
+      @FXML
+    private Stage stage;
+
+    @FXML
+    private Scene scene;
+
+    @FXML
+    private Parent root = null;
+
+      FXMLLoader loader;
 
     public void initialize (){
         ImageView imageView = new ImageView(getClass().getResource("/images/brush.png").toExternalForm());
@@ -32,6 +55,70 @@ public class PaintsController {
         logo.setPadding(Insets.EMPTY);
     }
 
+     static Watercolor watercolor = new Watercolor();
+    static Gouache gouache = new Gouache();
+    static PosterColors postercolor = new PosterColors();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        // ============== Watercolor ==================//
+        watercolor.setProductName("Watercolor");
+        name1.setText(watercolor.getProductName());
+
+        watercolor.setProductPrice(1200.00);
+        price1.setText(Double.toString(watercolor.getProductPrice()));
+
+        watercolor.setProductImage("images/watercolorpic.jpg");
+        Image watercolorpic = new Image(watercolor.getProductImage());
+        img1.setImage(watercolorpic);
+
+        // ============== Gouache ==================//
+
+        gouache.setProductName("Goauche");
+        name2.setText(gouache.getProductName());
+
+        gouache.setProductPrice(800.00);
+        price2.setText(Double.toString(gouache.getProductPrice()));
+
+        gouache.setProductImage("images/gouachepic.jpg");
+        Image gouachepic = new Image(gouache.getProductImage());
+        img2.setImage(gouachepic);
+
+        // ============== Poster Colors ==================//
+        postercolor.setProductName("Poster Color");
+        name3.setText(postercolor.getProductName());
+
+        postercolor.setProductPrice(500.00);
+        price3.setText(Double.toString(postercolor.getProductPrice()));
+
+        postercolor.setProductImage("images/postercolorpic.jpg");
+        Image postercolorpic = new Image(postercolor.getProductImage());
+        img3.setImage(postercolorpic);
+    }
+
+     public void buy(ActionEvent event) throws IOException {
+
+        AlertMaker.showSimpleAlert("notification", "item added successfully");
+
+        Button sourceButton = (Button) event.getSource();
+
+        // If addtocart button is pressed, set product status to true
+        if (sourceButton.equals(button1)) {
+            watercolor.setProductStatus(true);
+            checkoutController.addItem(checkoutController.pane1);
+        }
+
+        else if (sourceButton == button2) {
+            gouache.setProductStatus(true);
+            checkoutController.addItem(checkoutController.pane2);
+        }
+
+        else if (sourceButton == button3) {
+            postercolor.setProductStatus(true);
+            checkoutController.addItem(checkoutController.pane3);
+        }
+    }
 
     public void logo(ActionEvent event) throws IOException {
 
