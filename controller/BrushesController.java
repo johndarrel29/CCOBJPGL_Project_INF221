923ed2a5bfb025810178ee8cb70fc21a3bf38145
John Darrel Poquiz <111472863+johndarrel29@users.noreply.github.com>
2023-06-12 21:15:49 +0800

    Update BrushesController.java

--- controller/BrushesController.java
+++ controller/BrushesController.java
@@ -3,36 +3,14 @@ package controller;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class BrushesController {
     
-
-
-@FXML
-    Button logo;
-
-@FXML
-    Label name1;
-
-@FXML
-    private Stage stage;
-
-@FXML
-    private Scene scene;
-
-@FXML
-    private Parent root = null;
-
-    FXMLLoader loader;
- 
 
     public void logo(ActionEvent event) throws IOException {
 
@@ -43,4 +21,5 @@ public class BrushesController {
         stage.setScene(scene);
         stage.show();
     }
+
 }
