43bfac33b411064ecb00fa45b06922e2f4dc7678
theaneryy00 <111473139+theaneryy00@users.noreply.github.com>
2023-06-12 17:24:46 +0800

    Update HomeController.java

--- controller/HomeController.java
+++ controller/HomeController.java
@@ -3,7 +3,7 @@ package controller;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+//import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Node;
@@ -21,4 +21,26 @@ public class HomeController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void brush(ActionEvent event) throws IOException {
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Brushes.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void artpaper(ActionEvent event) throws IOException {
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Brushes.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    
 }
