cdb1eccd0f99be0feb5ae0133ae0530b082bef6f
John Darrel Poquiz <111472863+johndarrel29@users.noreply.github.com>
2023-06-14 22:07:27 +0800

    Update PaintsController.java

--- controller/PaintsController.java
+++ controller/PaintsController.java
@@ -9,32 +9,28 @@ import alert.AlertMaker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
-import javafx.scene.control.ContentDisplay;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 public class PaintsController implements Initializable {
 
     @FXML
-    Button logo, button1, button2, button3, cartbutton;;
+    Button logo, button1, button2, button3, button4, cartbutton;;
 
-     @FXML
-    Label name1, name2, name3, price1, price2, price3;
+    @FXML
+    Label name1, name2, name3, price1, price2, price3, price4;
 
-      @FXML
-    ImageView img1, img2, img3;
+    @FXML
+    ImageView img1, img2, img3, img4;
 
-      @FXML
+    @FXML
     private Stage stage;
 
     @FXML
@@ -43,20 +39,12 @@ public class PaintsController implements Initializable {
     @FXML
     private Parent root = null;
 
-      FXMLLoader loader;
+    FXMLLoader loader;
 
-    //for the home button lng to
-//     public void initialize (){
-//         ImageView imageView = new ImageView(getClass().getResource("/images/brush.png").toExternalForm());
-//         logo.setGraphic(imageView);
-//         imageView.fitWidthProperty().bind(logo.widthProperty().divide(2));
-//         imageView.setPreserveRatio(true);
-//         //Important otherwise button will wrap to text + graphic size (no resizing on scaling).
-//         logo.setMaxWidth(Double.MAX_VALUE);   
-//         logo.setPadding(Insets.EMPTY);
-//     }
+    @FXML
+    CheckoutController checkoutController = null;
 
-     static Watercolor watercolor = new Watercolor();
+    static Watercolor watercolor = new Watercolor();
     static Gouache gouache = new Gouache();
     static PosterColors postercolor = new PosterColors();
 
@@ -96,37 +84,60 @@ public class PaintsController implements Initializable {
         postercolor.setProductImage("images/postercolorpic.jpg");
         Image postercolorpic = new Image(postercolor.getProductImage());
         img3.setImage(postercolorpic);
+
+        try {
+            loader = new FXMLLoader(getClass().getResource("/view/Checkout.fxml"));
+            root = loader.load();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        //Clears all items in Checkout.fxml
+        checkoutController = loader.getController();
+        checkoutController.myvbox.getChildren().removeAll(checkoutController.myvbox.getChildren());
+    
     }
 
-     public void buy(ActionEvent event) throws IOException {
+    public void buy(ActionEvent event) throws IOException {
 
-        AlertMaker.showSimpleAlert("notification", "item added successfully");
+      AlertMaker.showSimpleAlert("notification", "item added successfully");
 
-        Button sourceButton = (Button) event.getSource();
+      Button sourceButton = (Button) event.getSource();
 
-        // If addtocart button is pressed, set product status to true
+        //If addtocart button is pressed, set product status to true
         if (sourceButton.equals(button1)) {
             watercolor.setProductStatus(true);
-            //checkoutController.addItem(checkoutController.pane1);
+            checkoutController.addItem(checkoutController.pane1);
         }
 
         else if (sourceButton == button2) {
             gouache.setProductStatus(true);
-            //checkoutController.addItem(checkoutController.pane2);
+            checkoutController.addItem(checkoutController.pane2);
         }
 
         else if (sourceButton == button3) {
             postercolor.setProductStatus(true);
-            //checkoutController.addItem(checkoutController.pane3);
+            checkoutController.addItem(checkoutController.pane3);
         }
     }
 
+    //home button
     public void logo(ActionEvent event) throws IOException {
 
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Home.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    // Goes to Checkout.fxml
+    public void gotocart(ActionEvent event) throws IOException {
+
+        Scene scene = new Scene(root);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Checkout.fxml"));
         stage.setScene(scene);
         stage.show();
     }
