9ac28e41ac15092c23b9f1ee0800dbc361071f83
John Darrel Poquiz <111472863+johndarrel29@users.noreply.github.com>
2023-06-14 23:54:14 +0800

    Update LoginController.java

--- controller/LoginController.java
+++ controller/LoginController.java
@@ -49,4 +49,14 @@ public class LoginController {
             mywarninglabel.setVisible(true);
         }
     }
+
+    public void logo(ActionEvent event) throws IOException {
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/HomePage.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
