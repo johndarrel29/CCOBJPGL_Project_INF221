3864eead88ab1fcd3fd5a2283ea8a2f624913ecd
John Darrel Poquiz <111472863+johndarrel29@users.noreply.github.com>
2023-06-15 20:27:53 +0800

    Add files via upload

---
+++ controller/LoginController.java
@@ -0,0 +1,53 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class LoginController {
+
+    @FXML
+    TextField mytextfield;
+
+    @FXML
+    PasswordField mypasswordfield;
+
+    @FXML
+    Button loginbutton;
+
+    @FXML
+    Label mywarninglabel;
+
+    @FXML
+    //HomeController homeController = null;
+
+    public void login(ActionEvent event) throws IOException {
+
+        String username = mytextfield.getText();
+        String password = mypasswordfield.getText();
+
+        if (username.equals("a") && password.equals("a")) {
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/HomePage.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } else {
+            mywarninglabel.setVisible(true);
+        }
+    }
+
+}
---
+++ model/AngularflatBrush.java
@@ -0,0 +1,5 @@
+package model;
+
+public class AngularflatBrush extends Product {
+
+}
