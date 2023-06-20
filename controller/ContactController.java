5396306e67b073529735cbf14b5caaeebd4548d1
Lyod Rhussel Torrenueva <111472869+MistMe@users.noreply.github.com>
2023-06-20 20:18:08 +0800

    Add files via upload

---
+++ controller/ContactController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import java.io.IOException;
+
+import alert.AlertMaker;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ContactController {
+    
+  public void logo(ActionEvent event) throws IOException {
+
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/HomePage.fxml"));
+    Parent root = loader.load();
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void send(ActionEvent event) throws IOException {
+
+    AlertMaker.showSuccessfulAlert("Your message have been sent!", " ");
+
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/HomePage.fxml"));
+    Parent root = loader.load();
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+    
+  }
+
+}
